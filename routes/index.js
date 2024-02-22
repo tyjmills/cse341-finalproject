44807ba6d7beb0930085d5ad9000864e884c0071
Tyler Mills <mil20064@byui.edu>
2024-02-21 19:00:01 -0700

    Added server.js and index.js files

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags=['Hello World']
+    res.send('Hello World');
+});
+
+router.use('/customers', require('./customers'));
+
+module.exports= router;
